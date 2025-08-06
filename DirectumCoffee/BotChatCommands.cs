242effd451e86e58de76debe349d4a7e0ef20d05
Dezodemius <gladkovyegor@gmail.com>
2025-08-06 14:11:19 +0400

    Исправления по stylecop для BotChatCommands

--- DirectumCoffee/BotChatCommands.cs
+++ DirectumCoffee/BotChatCommands.cs
@@ -1,19 +1,77 @@
 ﻿namespace DirectumCoffee;
 
-public class BotChatCommands
+/// <summary>
+/// Команды бота.
+/// </summary>
+internal static class BotChatCommands
 {
+    /// <summary>
+    /// Команда /start.
+    /// </summary>
     public const string Start = "/start";
+
+    /// <summary>
+    /// Команда /stop.
+    /// </summary>
     public const string Stop = "/stop";
+
+    /// <summary>
+    /// Команда /info.
+    /// </summary>
     public const string Info = "/info";
+
+    /// <summary>
+    /// Команда /change.
+    /// </summary>
     public const string Change = "/change";
+
+    /// <summary>
+    /// Команда /restart.
+    /// </summary>
     public const string Restart = "/restart";
+
+    /// <summary>
+    /// Команда /go.
+    /// </summary>
     public const string Go = "/go";
+
+    /// <summary>
+    /// Команда /changename.
+    /// </summary>
     public const string ChangeName = "/changename";
+
+    /// <summary>
+    /// Команда /changecity.
+    /// </summary>
     public const string ChangeCity = "/changecity";
+
+    /// <summary>
+    /// Команда /changework.
+    /// </summary>
     public const string ChangeWork = "/changework";
+
+    /// <summary>
+    /// Команда /changehobby.
+    /// </summary>
     public const string ChangeHobby = "/changehobby";
+
+    /// <summary>
+    /// Команда /changeinterests.
+    /// </summary>
     public const string ChangeInterests = "/changeinterests";
+
+    /// <summary>
+    /// Команда /generatepairs.
+    /// </summary>
     public const string GeneratePairs = "/generatepairs";
+
+    /// <summary>
+    /// Команда /sendpairs.
+    /// </summary>
     public const string SendPairs = "/sendpairs";
+
+    /// <summary>
+    /// Команда /randompair.
+    /// </summary>
     public const string RandomPair = "/randompair";
 }
